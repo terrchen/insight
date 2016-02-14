59e4d9a1a2f21a62e87fc9c80c90e81674a5f022
Terrence Chen <terrchen@gitsense.com>
2016-02-14 12:08:38 -0800

    Forgot to add config util file

---
+++ chrome/js/utils/config.js
@@ -0,0 +1,27 @@
+sdes.utils.config = function() {
+    var _this = this;
+
+    this.getRule = function() {
+        for ( var i = 0; i < sdes.config.page_rules.length; i++ ) {
+            var rule = sdes.config.page_rules[i],
+                patt = new RegExp(rule.matches.replace("*", ".*"));
+
+            if ( window.location.href.match(patt) )
+                return rule;
+        }
+
+        throw("GitSense: No rule found for current URL: "+window.location.href);
+    }
+
+    this.getHost = function() {
+        return _this.getRule().type;
+    }
+
+    this.getGitSenseApi = function() {
+        return _this.getRule().gitsense.api;
+    }
+
+    this.getGitSenseAccessToken = function() {
+        return _this.getRule().gitsense.secret;
+    }
+}
